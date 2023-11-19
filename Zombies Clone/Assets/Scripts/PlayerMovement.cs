4892ef95ffdc2a23cb033293066f5e4b79b2b5ed
Bjorn Nordenflycht <bjornnorden17@gmail.com>
2023-11-19 18:44:30 -0500

    starting wasd movement working

--- Zombies Clone/Assets/Scripts/PlayerMovement.cs
+++ Zombies Clone/Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,25 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer)
+        {
+            if (playerVelocity.y < 0)
+                playerVelocity.y = 0f;
+        }
+
+        Vector3 move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y = jumpHeight;
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
